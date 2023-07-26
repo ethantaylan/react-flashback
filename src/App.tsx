84ef93ab9c1284b2eb39566b6fb8a4c963eb440a
Ethan <ethtaylan@gmail.com>
2023-07-26 21:15:14 +0200

    fix: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Navbar } from "./components/navbar";
-import { Modal } from "./modals/modals";
 
 export const App: React.FC = () => {
-  return (
-    <div className="w-screen h-screen">
-      <Modal />
-      <Navbar />
-      <button onClick={() => window.firstModal.showModal() as HTMLFormElement}>
-        open modal
-      </button>
-    </div>
-  );
+  return <div className="w-screen h-screen">hello world</div>;
 };
